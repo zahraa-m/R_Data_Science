d3ff0199f0ce7f3b7cb5c760a8ce8951b3615f4a
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-11-15 23:33:28 +0000

    Update 12-Normal_Distribution.R

--- 12-Normal_Distribution.R
+++ 12-Normal_Distribution.R
@@ -5,3 +5,9 @@ data(heights)
 x <- heights$height[heights$sex == "Male"]
 mean(x>69 & x<= 72)
 
+#Use the normal approximation to estimate the proportion
+#the proportion of the data that is between 69 and 72 inches.
+avg <- mean(x)
+stdev <- sd(x)
+pnorm(72,avg, stdev) - pnorm(69,avg, stdev)
+
