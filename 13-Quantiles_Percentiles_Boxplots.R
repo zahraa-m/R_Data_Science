a29455fbe9e6f768f873428396fa988493217695
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-11-17 01:08:06 +0000

    Update 13-Quantiles_Percentiles_Boxplots.R

--- 13-Quantiles_Percentiles_Boxplots.R
+++ 13-Quantiles_Percentiles_Boxplots.R
@@ -6,3 +6,10 @@ female <- heights$height[heights$sex=="Female"]
 length(male)
 length(female)
 
+#Create two five row vectors showing the 10th, 30th, 50th, 70th, and 90th percentiles for the heights of each sex
+female_percentiles <- quantile(female, seq(0.1, 0.9, 0.2))
+male_percentiles <- quantile(male, seq(0.1, 0.9, 0.2))
+
+#create a data frame called df with these two vectors as columns.
+df <- data.frame(female=female_percentiles , male=male_percentiles)
+df
